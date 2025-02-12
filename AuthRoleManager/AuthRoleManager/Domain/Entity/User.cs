c7cdf5f7ba934c04cfe2761d5739b9258a3cd131
Zuxriddin-Zarifov <zuxriddinzarifov25@gmail.com>
2025-02-12 12:30:54 +0500

    database table schema name and connection string created

--- AuthRoleManager/AuthRoleManager/Domain/Entity/User.cs
+++ AuthRoleManager/AuthRoleManager/Domain/Entity/User.cs
@@ -1,14 +1,15 @@
 ﻿using AuthRoleManager.Domain.Enum;
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace AuthRoleManager.Domain.Entity
+namespace AuthRoleManager.Domain.Entity;
+
+[Table("users",Schema ="auth_role_manager")]
+public class User : ModelBase
 {
-    public class User : ModelBase
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public Role Role { get; set; } = Role.User;
-    }
+   [Column("first_name")] public string FirstName { get; set; }
+   [Column("last_name")] public string LastName { get; set; }
+   [Column("email")] public string Email { get; set; }
+   [Column("password")] public string Password { get; set; }
+   [Column("role")] public Role Role { get; set; } = Role.User;
 }
